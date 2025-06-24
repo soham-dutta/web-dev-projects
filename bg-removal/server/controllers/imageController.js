41bb18abf5f2de27406b7f5383a306f87869a27c
soham <sohamdutta.sd@gmail.com>
2025-06-25 02:33:15 +0530

    Added Origins

--- bg-removal/server/controllers/imageController.js
+++ bg-removal/server/controllers/imageController.js
@@ -3,57 +3,44 @@ import fs from 'fs'
 import FormData from 'form-data'
 import userModel from '../models/userModel.js'
 
-// Controller function to remove bg from image
-// http://localhost:4000/api/image/remove-bg
 const removeBgImage = async (req, res) => {
-
   try {
+    const clerkId = req.clerkId;
 
-    const { clerkId } = req.body
-
-    // Fetching User Details Using ClerkId
-    const user = await userModel.findOne({ clerkId })
+    const user = await userModel.findOne({ clerkId });
     if (!user) {
-      return res.json({ success: false, message: 'User Not Found' })
+      return res.json({ success: false, message: 'User Not Found' });
     }
 
-    // Checking User creditBalance
     if (user.creditBalance === 0) {
-      return res.json({ success: false, message: 'No Credit Balance', creditBalance: user.creditBalance })
+      return res.json({ success: false, message: 'No Credit Balance', creditBalance: user.creditBalance });
     }
 
-    // Getting Image Path
     const imagePath = req.file.path;
+    const imageFile = fs.createReadStream(imagePath);
 
-    // Read the image file
-    const imageFile = fs.createReadStream(imagePath)
+    const formdata = new FormData();
+    formdata.append('image_file', imageFile);
 
-    // Creation of new multi/part formdata
-    const formdata = new FormData()
-    formdata.append('image_file', imageFile)
-
-    // Calling Clipdrop API
     const { data } = await axios.post('https://clipdrop-api.co/remove-background/v1', formdata, {
       headers: {
         'x-api-key': process.env.CLIPDROP_API,
+        ...formdata.getHeaders()
       },
-      responseType: "arraybuffer"
-    })
+      responseType: 'arraybuffer'
+    });
 
-    // Convertion of arrayBuffer to base64
     const base64Image = Buffer.from(data, 'binary').toString('base64');
-    const resultImage = `data:${req.file.mimetype};base64,${base64Image}`
+    const resultImage = `data:${req.file.mimetype};base64,${base64Image}`;
 
-    // Deduction of user credit 
-    await userModel.findByIdAndUpdate(user._id, { creditBalance: user.creditBalance - 1 })
+    await userModel.findByIdAndUpdate(user._id, { creditBalance: user.creditBalance - 1 });
 
-    // Sending Response
-    res.json({ success: true, message: "Background Removed", resultImage, creditBalance: user.creditBalance - 1 })
+    res.json({ success: true, message: "Background Removed", resultImage, creditBalance: user.creditBalance - 1 });
 
   } catch (error) {
-    console.log(error.message)
-    res.json({ success: false, message: error.message })
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
   }
-}
+};
 
-export { removeBgImage }
+export { removeBgImage };
--- bg-removal/server/middlewares/auth.js
+++ bg-removal/server/middlewares/auth.js
@@ -1,31 +1,24 @@
 import jwt from 'jsonwebtoken'
 
-// Middleware Function to decode jwt token to get clerkId
-// add customize session token in clerk dashboard // {"clerkId": "{{user.id}}"}
 const authUser = async (req, res, next) => {
+  try {
+    const token = req.headers['authorization'] || req.headers['token'];
 
-    try {
-
-        // getting token from headers
-        const { token } = req.headers;
-
-        // checking token availability
-        if (!token) {
-            return res.json({ success: false, message: 'Not Authorized Login Again' })
-        }
-
-        // decoding JWT token
-        const token_decode = jwt.decode(token)
-
-        // getting clerkId from decoded token
-        req.body.clerkId = token_decode.clerkId
-        next()
-
-    } catch (error) {
-        console.log(error.message)
-        res.json({ success: false, message: error.message })
+    if (!token) {
+      return res.json({ success: false, message: 'Not Authorized Login Again' });
     }
 
-}
+    const token_decode = jwt.decode(token);
+    if (!token_decode?.clerkId) {
+      return res.json({ success: false, message: 'Invalid Token' });
+    }
 
-export default authUser
+    req.clerkId = token_decode.clerkId;
+    next();
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export default authUser;
